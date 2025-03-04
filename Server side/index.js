47ed940ed6627d42081925cdf7c6054e453aa76a
Annette <annetaliya@gmail.com>
2025-03-04 10:33:26 -0800

    implement post routing

--- Server side/index.js
+++ Server side/index.js
@@ -1,38 +1,13 @@
 const express = require('express');
-
 const app = express();
 const PATH = 8000;
-const db = require('./database.js')
+const userRoutes = require('./Routes/userRoutes.js')
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.get('/users', (req, res, next) => {
-    const sql = "select * from user "
-    const params = [];
-    db.all(sql, params, (err, rows) => {
-        if (err) {
-            res.status(400).json({"error":err.message })
-            return;
-        }
-        res.json({
-            "message": "OK",
-            "data":rows
-        })
-    })
-    
-})
+app.use('/users', userRoutes);
 
-app.get('/users/:id', (req, res, next) => {
-    const sql = 'select * from user where id = ?'
-    const params = [req.params.id]
-    db.get(sql, params, (err, row) => {
-        if (err) {
-            res.status(400).json({"error":err.message})
-            return;
-        }
-        res.json({
-            "message": "ok",
-            "data": row
-        })
-    })
-})
+
 
 app.listen(PATH , () => {console.log(`app is listening on port ${PATH}`)})
