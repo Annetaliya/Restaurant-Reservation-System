0454906f12c6242c2fda35318ab826ec21690d55
Annette <annetaliya@gmail.com>
2025-05-30 10:15:16 -0700

    add environment variables for supabase

--- Server side/Routes/subscription.js
+++ Server side/Routes/subscription.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const route = express.Router();
 const { v4: uuidv4 } = require('uuid')
-const { getDB } = require('../database2.js');
+const supabase = require('../supaBaseClient')
 
 
 route.post('/', async (req, res) => {
     const subscriptions = JSON.stringify(req.body);
     const id = uuidv4();
-    const db = getDB();
+    
 
     try {
-        await db.execute(`INSERT INTO subscriptions (id, subscriptions) VALUES (?,?)`, [id, subscriptions])
+        const { error } =  await supabase
+            .from('subscriptions')
+            .insert([{ id, subscriptions }])
+        if (error) {
+           console.error('Subscription insert failed:', error.message);
+            return res.status(500).json({ error: 'Database Error' }); 
+        }
         res.json({
             message: 'Subscription Saved'
 
