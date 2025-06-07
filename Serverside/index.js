83892fe2e4b0f33e08ded90eae5be80b56f718bd
Annette <annetaliya@gmail.com>
2025-06-07 14:07:06 -0700

    cors url

--- Serverside/index.js
+++ Serverside/index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://restaurant-reservation-system-doafu7qaz.vercel.app/',
     credentials: true,              
   }));
 //jwt
