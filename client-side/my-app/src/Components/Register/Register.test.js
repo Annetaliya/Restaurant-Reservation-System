039115ecd6e8d393f7c1ebc05fb3f88b8153fdef
Annetaliya <annetaliya@gmail.com>
2026-02-05 12:24:26 +0300

    test foor error message on incorrect password match

--- client-side/my-app/src/Components/Register/Register.test.js
+++ client-side/my-app/src/Components/Register/Register.test.js
@@ -1,5 +1,6 @@
 import {render, screen, fireEvent } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import Register from './Register'
 
 test('renders all form fields', () => {
@@ -16,4 +17,17 @@ test('renders all form fields', () => {
     expect(screen.getByPlaceholderText(/confirm password/i)).toBeInTheDocument();
     expect(screen.getByPlaceholderText(/enter phone no/i)).toBeInTheDocument();
     expect(screen.getByRole('button', {name: /submit/i})).toBeInTheDocument();
+})
+
+test('shows error when passwords do not match', async () => {
+    render(
+        <MemoryRouter>
+            <Register />
+        </MemoryRouter>
+    )
+    await userEvent.type(screen.getByPlaceholderText(/enter password/i), 'password123');
+    await userEvent.type(screen.getByPlaceholderText(/confirm password/i), 'password321');
+    fireEvent.blur(screen.getByPlaceholderText(/confirm password/i));
+
+    expect(await screen.findByText(/passwords must match/i)).toBeInTheDocument()
 })
