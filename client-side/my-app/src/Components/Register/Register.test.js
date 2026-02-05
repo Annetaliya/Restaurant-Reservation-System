d5b80993a972596c1f60ba9e846d404994e2aa6f
Annetaliya <annetaliya@gmail.com>
2026-02-05 12:09:39 +0300

    test for registeration form all fields are rendered

---
+++ client-side/my-app/src/Components/Register/Register.test.js
@@ -0,0 +1,19 @@
+import {render, screen, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Register from './Register'
+
+test('renders all form fields', () => {
+    render(
+        <MemoryRouter>
+            <Register />
+        </MemoryRouter>
+
+    )
+    expect(screen.getByPlaceholderText(/first name/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/last name/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/enter password/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/confirm password/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/enter phone no/i)).toBeInTheDocument();
+    expect(screen.getByRole('button', {name: /submit/i})).toBeInTheDocument();
+})
